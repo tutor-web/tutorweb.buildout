38282dd25e06ab7991f0abfbef562d869d992bfc
Jamie Lentin <jm@lentin.co.uk>
2021-06-10 12:01:00 +0100

    cfgs: Upgrade to 4.3.20

---
+++ cfgs/fetch.sh
@@ -0,0 +1,12 @@
+CFG_URL=${1-https://dist.plone.org/release/4.3.20/versions.cfg}
+OUT_NAME="$(echo ${CFG_URL} | sed 's/\W/./g')"
+mkdir -p cfgs/
+
+echo $CFG_URL "${OUT_NAME}" 1>&2
+curl -sL "${CFG_URL}" > "cfgs/${OUT_NAME}"
+
+for SUB_URL in $(grep -oE 'https?://.*\.cfg' "cfgs/${OUT_NAME}"); do
+    SUB_OUT_NAME="$($0 "${SUB_URL}")"
+    sed -i "s|${SUB_URL}|${SUB_OUT_NAME}|" "cfgs/${OUT_NAME}"
+done
+echo "${OUT_NAME}"
